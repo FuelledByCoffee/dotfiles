82cfdbda1286e4e37d6ceb03526876d7ab624104
Johannes Eckhoff <j.eckhoff@gmail.com>
2021-04-18 19:33:57 +0200

    simplify with brew --prefix

--- fzf.zsh
+++ fzf.zsh
@@ -1,21 +1,13 @@
 # Setup fzf
 # ---------
-if [[ $(uname -m) == "arm64"* ]]; then
-  prefix="/opt/homebrew/opt"
-elif [[ "$OSTYPE" == "linux"* ]]; then
-  prefix="~/home/linuxbrew/.linuxbrew"
-else
-  prefix="/usr/local"
-fi
-
-if [[ ! "$PATH" == "*${prefix}/fzf/bin*" ]]; then
-  export PATH="${PATH:+${PATH}:}${prefix}/fzf/bin"
+if [[ ! "$PATH" == "*$(brew --prefix)/opt/fzf/bin*" ]]; then
+  export PATH="${PATH:+${PATH}:}/$(brew --prefix)/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${prefix}/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/${prefix}/fzf/shell/key-bindings.zsh"
+source "/$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
