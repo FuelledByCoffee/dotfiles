9bd90becd986a2188edd017a94f0a87f84cd7af7
Johannes Eckhoff <j.eckhoff@gmail.com>
2022-04-02 18:45:35 +0200

    Update binds

--- vim-config/lua/lsp-config.lua
+++ vim-config/lua/lsp-config.lua
@@ -1,6 +1,15 @@
 
 local custom_lsp_attach = function(client)
   -- See `:help nvim_buf_set_keymap()` for more information
+  vim.keymap.set("n", "K",  vim.lsp.buf.hover,            { buffer=0 })
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition,       { buffer=0 })
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition,  { buffer=0 })
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation,   { buffer=0 })
+  vim.keymap.set("n", "-r", vim.lsp.buf.rename,           { buffer=0 })
+  vim.keymap.set("n", "-.", vim.lsp.buf.code_action,      { buffer=0 })
+  vim.keymap.set("n", "-n", vim.diagnostic.goto_next,     { buffer=0 })
+  vim.keymap.set("n", "-p", vim.diagnostic.goto_prev,     { buffer=0 })
+  vim.keymap.set("n", "-l", "<cmd>Telescope diagnostics<cr>", { buffer=0 })
 
 
   -- Use LSP as the handler for omnifunc.
@@ -18,13 +27,15 @@ end
 -- connect to server
 require('lspconfig').clangd.setup{
   on_attach = function() print("Attached")
-    vim.keymap.set("n", "K",  vim.lsp.buf.hover,           { buffer=0 })
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition,      { buffer=0 })
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer=0 })
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation,  { buffer=0 })
-    vim.keymap.set("n", "-n", vim.diagnostic.goto_next,    { buffer=0 })
-    vim.keymap.set("n", "-p", vim.diagnostic.goto_prev,    { buffer=0 })
-    vim.keymap.set("n", "-p", vim.diagnostic.goto_prev,    { buffer=0 })
+    vim.keymap.set("n", "K",  vim.lsp.buf.hover,            { buffer=0 })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition,       { buffer=0 })
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition,  { buffer=0 })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation,   { buffer=0 })
+    vim.keymap.set("n", "-r", vim.lsp.buf.rename,           { buffer=0 })
+    vim.keymap.set("n", "-.", vim.lsp.buf.code_action,      { buffer=0 })
+    vim.keymap.set("n", "-n", vim.diagnostic.goto_next,     { buffer=0 })
+    vim.keymap.set("n", "-p", vim.diagnostic.goto_prev,     { buffer=0 })
+    vim.keymap.set("n", "-l", "<cmd>Telescope diagnostics<cr>", { buffer=0 })
   end,
 }
 
