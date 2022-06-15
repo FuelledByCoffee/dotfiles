3680679cc3d105baa118a54ea25a90aaea3ec482
Johannes Eckhoff <j.eckhoff@gmail.com>
2022-06-15 10:19:11 +0200

    Remove leading forward slash

--- fzf.zsh
+++ fzf.zsh
@@ -7,7 +7,7 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/$prefix/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$prefix/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
