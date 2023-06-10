6b57c5109281947cf2b3774fbecd6f69d564e55b
Johannes Eckhoff <j.eckhoff@gmail.com>
2023-06-10 16:43:47 +0200

    New oh-my-posh activation
    oh-my-posh changed the way you set a theme

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,6 +1,5 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme half-life # half-life
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/half-life.omp.json" | Invoke-Expression
 
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 Set-PSReadlineOption -EditMode vi
