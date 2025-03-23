ce66791ca6c416009aafcef4c72321dddd5b08b9
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-03-23 12:36:29 +0100

    Add fuzzy git history search

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -84,6 +84,23 @@ function rm-env {
     | sed 's/=.*//')
 }
 
+# https://github.com/rothgar/mastering-zsh/blob/master/docs/helpers/functions.md#interactive-git-diff
+function fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" \
+    | fzf --ansi --preview "echo {} \
+    | grep -o '[a-f0-9]\{7\}' \
+    | head -1 \
+    | xargs -I % sh -c 'git show --color=always %'" \
+        --bind "enter:execute:
+            (grep -o '[a-f0-9]\{7\}' \
+                | head -1 \
+                | xargs -I % sh -c 'git show --color=always % \
+                | less -R') << 'FZF-EOF'
+            {}
+    FZF-EOF"
+}
+
 
 bindkey '^p' fzf-cd-widget
 
