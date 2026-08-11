fbcd3fff5f210f261cb77bf31d367320153b2622
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-11 10:11:50 +0200

    Fix light theme for fzf bat

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -12,6 +12,14 @@ fi
 # ------------
 source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
 
+# Check if macOS is currently set to dark mode
+if defaults read -g AppleInterfaceStyle &>/dev/null; then
+	bat_theme="Catppuccin Macchiato"
+else
+	bat_theme="Catppuccin Latte"
+fi
+bat_preview=" bat --style=numbers --color=always --theme=\"$bat_theme\" --line-range :500"
+
 export FZF_DIR="$HOMEBREW_PREFIX/opt/fzf"
 export FZF_DEFAULT_COMMAND="fd --hidden --follow --exclude .git"
 export FZF_DEFAULT_OPTS="
@@ -25,18 +33,30 @@ export FZF_DEFAULT_OPTS="
   --bind 'ctrl-f:change-prompt(Files❯ )+reload(fd -t f)'
   --bind 'ctrl-d:change-prompt(Directories❯ )+reload(fd -t d)'
   --bind 'ctrl-r:change-prompt(All❯ )+reload(fd)'
-  --bind 'ctrl-e:execute(echo {+} | xargs -o $VISUAL)'
+  --bind 'ctrl-e:execute(echo {+} | xargs -o \$VISUAL)'
   --header='C-r reset | C-d Directories | C-f files | C-e edit | ? toggle preview'
-  --preview '([[ -f {} ]] && (bat --style=plain --color=always {} \
+  --preview '([[ -f {} ]] && ($bat_preview {} \
   || cat {})) \
-  || ([[ -d {} ]] && (exa -T 2> /dev/null {}) \
-  || (ls {})) \
+  || ([[ -d {} ]] && (tree -C 2> /dev/null {} || ls -p {})) \
   || echo {} 2> /dev/null | head -200'"
 
+# Search for files and folders within the current directory
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS=""
-# export FZF_CTRL_R_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_R_OPTS="--preview-window=:hidden"
+export FZF_CTRL_R_COMMAND="$FZF_DEFAULT_COMMAND"
+# export FZF_CTRL_R_OPTS="--preview-window=:hidden"
+# Syncs your command history search to match your premium visual layout
+export FZF_CTRL_R_OPTS="
+  --height 95%
+  --layout=reverse
+  --pointer='▶ '
+  --margin=1
+  --padding=1
+  --prompt='History❯ '
+  --header='Press Enter to run | Ctrl-Y to copy command'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --preview 'echo {2..} | $bat_preview -l bash'
+  --preview-window='right:50%:wrap'"
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
 export FZF_ALT_C_OPTS=""
 
