2a4dd1efe6da90301eac29514bad1f790ef7315b
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-11 10:27:43 +0200

    Formatting

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,12 +1,12 @@
 # Setup fzf
 # ---------
 if [[ ":$PATH:" != *":$HOMEBREW_PREFIX/opt/fzf/bin:"* ]]; then
-  path+=($HOMEBREW_PREFIX/opt/fzf/bin)
+	path+=($HOMEBREW_PREFIX/opt/fzf/bin)
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" 2>/dev/null
 
 # Key bindings
 # ------------
@@ -64,55 +64,50 @@ export FZF_ALT_C_OPTS=""
 # export _ZO_FZF_OPTS="
 #   --height 95%"
 
-
 # command for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
 function _fzf_compgen_path {
-  fd --hidden --follow --exclude ".git" . "$1"
+	fd --hidden --follow --exclude ".git" . "$1"
 }
-
 
 # Use fd to generate the list for directory completion
 function _fzf_compgen_dir {
-  fd --type d --hidden --follow --exclude ".git" . "$1"
+	fd --type d --hidden --follow --exclude ".git" . "$1"
 }
-
 
 # fh - search in your command history and execute selected command
 function fh {
-  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) \
-    | fzf +s --tac --no-preview \
-    | sed 's/ *[0-9]* *//')
+	eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) |
+		fzf +s --tac --no-preview |
+		sed 's/ *[0-9]* *//')
 }
-
 
 # Search for and kill process
 function hunt {
-  ps -ef \
-    | fzf --no-preview --bind 'ctrl-r:reload(ps -ef)' --header 'Press CTRL-R to reload' \
-    | awk '{print $2}' \
-    | xargs kill -9
+	ps -ef |
+		fzf --no-preview --bind 'ctrl-r:reload(ps -ef)' --header 'Press CTRL-R to reload' |
+		awk '{print $2}' |
+		xargs kill -9
 }
-
 
 # Search for environment variable to unset
 function rm-env {
-  unset $(env \
-    | fzf --no-preview --bind 'ctrl-r:reload(env)' --header 'Press CTRL-R to reload' \
-    | sed 's/=.*//')
+	unset $(env |
+		fzf --no-preview --bind 'ctrl-r:reload(env)' --header 'Press CTRL-R to reload' |
+		sed 's/=.*//')
 }
 
 # Search git commits
 # https://github.com/rothgar/mastering-zsh/blob/master/docs/helpers/functions.md#interactive-git-diff
 function fshow() {
-  git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" \
-    | fzf --ansi --preview "echo {} \
+	git log --graph --color=always \
+		--format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+		fzf --ansi --preview "echo {} \
     | grep -o '[a-f0-9]\{7\}' \
     | head -1 \
     | xargs -I % sh -c 'git show --color=always %'" \
-        --bind "enter:execute:
+			--bind "enter:execute:
             (grep -o '[a-f0-9]\{7\}' \
                 | head -1 \
                 | xargs -I % sh -c 'git show --color=always % \
@@ -121,6 +116,4 @@ function fshow() {
     FZF-EOF"
 }
 
-
 bindkey '^p' fzf-cd-widget
-
