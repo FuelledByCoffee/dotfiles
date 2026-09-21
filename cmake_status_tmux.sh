a50ce3957c7d9167de678b2ac58623e34eaaa868
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-09-21 18:19:08 +0200

    feat: cmake version and buildtype

--- cmake_status_tmux.sh
+++ cmake_status_tmux.sh
@@ -6,6 +6,8 @@ if [ ! -f "CMakeLists.txt" ]; then
     exit 0
 fi
 
+cmake_version=$(cmake --version | awk 'NR==1 {print $3}')
+
 # -I = Search ignored folders (like build/)
 # -H = Search hidden folders (like .build/)
 # -max-depth 2 = Look exactly up to 1 subdirectory deep relative to here
@@ -14,5 +16,7 @@ build_types=$(fd -IH --fixed-strings --max-depth 2 "CMakeCache.txt" -X \
 
 # Only print if a build type string was successfully pulled out
 if [ -n "$build_types" ]; then
-    echo "#[fg=#0012dd]⟁  $build_types#[default]"
+    echo "#[fg=${cmake_blue}]#[fg=#fdfdfd,bg=${cmake_blue}] \
+		cmake v${cmake_version}: $build_types #[default,fg=${cmake_blue}]#[default]   "
 fi
+#   
