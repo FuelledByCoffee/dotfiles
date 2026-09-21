6704abe2860e5a304a6ded89912039d27e0329d1
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-09-21 11:47:11 +0200

    feat: cmake build type in tmux status

---
+++ cmake_status_tmux.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd "$1" 2>/dev/null || exit
+
+# Exit if this directory is not a CMake project root
+if [ ! -f "CMakeLists.txt" ]; then
+    exit 0
+fi
+
+# -I = Search ignored folders (like build/)
+# -H = Search hidden folders (like .build/)
+# -max-depth 2 = Look exactly up to 1 subdirectory deep relative to here
+build_types=$(fd -IH --fixed-strings --max-depth 2 "CMakeCache.txt" -X \
+    awk -F= '/^CMAKE_BUILD_TYPE:STRING=/ {print $2}' | sort -u | paste -sd, -)
+
+# Only print if a build type string was successfully pulled out
+if [ -n "$build_types" ]; then
+    echo "#[fg=#0012dd]⟁ $build_types#[default]"
+fi
